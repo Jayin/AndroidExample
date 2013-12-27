eb09886525db7b15bacd51f7a7aa1307e9eab697
Jayin Ton <tonjayin@gmail.com>
2013-12-27 14:53:01 +0800

    add sample of PullAndLoadMoreListView

--- src/com/example/android_service/PullAndLoadMoreListViewTest.java
+++ src/com/example/android_service/PullAndLoadMoreListViewTest.java
@@ -5,15 +5,21 @@ import java.util.LinkedList;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 
-import com.example.widget.MyPullToRefrushListView;
-import com.example.widget.MyPullToRefrushListView.OnRefreshListener;
+import com.costum.android.widget.PullAndLoadListView;
+import com.costum.android.widget.PullAndLoadListView.OnLoadMoreListener;
+import com.costum.android.widget.PullToRefreshListView.OnRefreshListener;
+import com.utils.AndroidUtils;
 
 public class PullAndLoadMoreListViewTest extends BaseUIActivity {
 	// list with the data to show in the listview
 	private LinkedList<String> mListItems;
-	private MyPullToRefrushListView xlv;
+	private PullAndLoadListView xlv;
+	private ArrayAdapter<String> adapter;
 
 	// The data to be displayed in the ListView
 	private String[] mNames = { "Fabian", "Carlos", "Alex", "Andrea", "Karla",
@@ -29,23 +35,43 @@ public class PullAndLoadMoreListViewTest extends BaseUIActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.acty_pullandloadlv);
-		xlv = (MyPullToRefrushListView) findViewById(R.id.xlistview);
+		xlv = (PullAndLoadListView) findViewById(R.id.xlistview);
 
 		mListItems = new LinkedList<String>();
 		mListItems.addAll(Arrays.asList(mNames));
-		mListItems.addAll(Arrays.asList(mAnimals));
+		// mListItems.addAll(Arrays.asList(mAnimals));
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.item_lv, mListItems);
+		adapter = new ArrayAdapter<String>(this, R.layout.item_lv, mListItems);
 		xlv.setAdapter(adapter);
-        xlv.setOnRefreshListener(new OnRefreshListener() {
-			
+		xlv.setOnRefreshListener(new OnRefreshListener() {
+
 			@Override
 			public void onRefresh() {
-				new Refresh().execute();
+				toast("onRefresh");
+				mListItems.addAll(Arrays.asList(mAnimals));
+				adapter.notifyDataSetChanged();
+				//xlv.onRefreshComplete("2013.12.22");
+			}
+		});
+
+		xlv.setOnLoadMoreListener(new OnLoadMoreListener() {
+
+			public void onLoadMore() {
+
+				new Load().execute();
+			}
+		});
+
+		xlv.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				toast("you click:"+position);
 				
 			}
 		});
+
 	}
 
 	@Override
@@ -74,6 +100,43 @@ public class PullAndLoadMoreListViewTest extends BaseUIActivity {
 		protected void onPostExecute(Void result) {
 			toast("刷新完毕");
 			xlv.onRefreshComplete();
+		}
+
+	}
+
+	class Load extends AsyncTask<Void, Void, Boolean> {
+
+		@Override
+		protected Boolean doInBackground(Void... params) {
+			try {
+				Thread.sleep(2000);
+				
+				if (!AndroidUtils.isNetworkConnected(getContext()))
+					throw new Exception("网络异常！");
+				else{
+					mListItems.addAll(Arrays.asList(mAnimals));
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+			return true;
+		}
+
+		@Override
+		protected void onPostExecute(Boolean result) {
+			if (result) {
+				toast("Load完毕");
+				adapter.notifyDataSetChanged();
+				xlv.onLoadMoreComplete();
+			} else {
+				toast("网络不给力啊");
+				xlv.onLoadMoreComplete();
+			}
+
 		}
 
 	}
